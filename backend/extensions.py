a94d19f3b90813caa69e7e2cf42574b50ef27756
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-09-22 15:44:15 +0300

    Organize the imports in seed and update the extensions in extensions

--- backend/extensions.py
+++ backend/extensions.py
@@ -13,6 +13,6 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-db      = SQLAlchemy()
-cors    = CORS()
+db = SQLAlchemy()
+cors = CORS()
 migrate = Migrate()
